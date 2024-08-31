7ae44a6bed48c535aa0cbe3ebf6ffa8cb7c7e42e
Satoru <aubinbot@gmail.com>
2024-08-31 21:23:43 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -38,8 +38,8 @@ global.prefa = '.'
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
-global.autoread = true //auto read messages
-global.autobio = true //auto update bio
+global.autoread = false //auto read messages
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = false //auto view status/story
 
